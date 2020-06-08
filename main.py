daa8eacbb2ae3addadc77184eda2af04d2627107
alexeby <ebyy22@gmail.com>
2020-06-07 22:10:05 -0500

    logging and initializing configurations

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-import cardcollection
+import logging
+import os
+import configparser
+from logging import config
 
+# Initialize the log configuration file
+logging.config.fileConfig(fname='init/log.ini', disable_existing_loggers=False)
+logger = logging.getLogger('testing')
+logger.info('Starting image download process')
+
+# Initialize configuration file
+parser = configparser.ConfigParser()
+parser.read('init/conf.ini')
+SITE_URL = parser.get('Websites', 'image_search_site')
+IMG_DOWNLOAD = parser.get('Paths', 'img_download')
+DATA_FILE_PATH = parser.get('Paths', 'data_dir')
+logger.info('SITE_URL: ' + SITE_URL)
+logger.info('IMG_DOWNLOAD: ' + IMG_DOWNLOAD)
+logger.info('DATA_FILE_PATH: ' + DATA_FILE_PATH)
+
+# Create download directory if not already exists
+try:
+    os.makedirs(IMG_DOWNLOAD, exist_ok=True)
+except OSError as e:
+    logger.exception(e)
+    raise Exception
 
 
 def main():
