bf7bf94218b14f67b6e7f3cee72df9162980413e
alexeby <ebyy22@gmail.com>
2020-06-10 12:28:13 -0500

    import of card objects from csv format

--- cardcollection/dataimport.py
+++ cardcollection/dataimport.py
@@ -1,5 +1,9 @@
-from cardcollection.card import Card
 import csv
+import logging
+
+from cardcollection.card import *
+
+logger = logging.getLogger(__name__)
 
 
 # Data import from CSV with format of name,monster_type,attribute,level,attack,defense,edition,set_number,pass_code,condition,description
@@ -8,72 +12,59 @@ class DataImport:
     def __init__(self, file_location=None):
         self._file_location = file_location
 
-    def init_indexes(self, header):
+    @staticmethod
+    def init_indexes(header):
         iteration = 0
-        name = None
-        monster_type = None
-        attribute = None
-        level = None
-        attack = None
-        defense = None
-        edition = None
-        set_number = None
-        pass_code = None
-        condition = None
-        description = None
+        index_dict = {'name': None,
+                      'monster_type': None,
+                      'attribute': None,
+                      'level': None,
+                      'attack': None,
+                      'defense': None,
+                      'pass_code': None,
+                      'description': None,
+                      'set_number': None,
+                      'edition': None,
+                      'rarity': None,
+                      'condition': None}
 
         for column in header:
-            if column == 'name':
-                name = iteration
-            elif column == 'monster_type':
-                monster_type = iteration
-            elif column == 'attribute':
-                attribute = iteration
-            elif column == 'level':
-                level = iteration
-            elif column == 'attack':
-                attack = iteration
-            elif column == 'defense':
-                defense = iteration
-            elif column == 'edition':
-                edition = iteration
-            elif column == 'set_number':
-                set_number = iteration
-            elif column == 'pass_code':
-                pass_code = iteration
-            elif column == 'condition':
-                condition = iteration
-            elif column == 'description':
-                description = iteration
+            if column == 'name': index_dict['name'] = iteration
+            elif column == 'monster_type': index_dict['monster_type'] = iteration
+            elif column == 'attribute': index_dict['attribute'] = iteration
+            elif column == 'level': index_dict['level'] = iteration
+            elif column == 'attack': index_dict['attack'] = iteration
+            elif column == 'defense': index_dict['defense'] = iteration
+            elif column == 'pass_code': index_dict['pass_code'] = iteration
+            elif column == 'description': index_dict['description'] = iteration
+            elif column == 'set_number': index_dict['set_number'] = iteration
+            elif column == 'edition': index_dict['edition'] = iteration
+            elif column == 'rarity': index_dict['rarity'] = iteration
+            elif column == 'condition': index_dict['condition'] = iteration
             iteration = iteration + 1
 
-        return [name, monster_type, attribute, level, attack, defense, edition,
-                set_number, pass_code, condition, description]
+        return index_dict
 
-    def define_card_object(self, indexes, card_info):
+    def define_card_object(self, indexes: dict, card_info: list):
         card_object = Card()
-        if indexes[0] is not None:
-            card_object.set_name(card_info[indexes[0]])
-        elif indexes[1] is not None:
-            card_object.set_monster_type(card_info[indexes[1]])
-        elif indexes[2] is not None:
-            card_object.set_attribute(card_info[indexes[2]])
-        elif indexes[3] is not None:
-            card_object.set_level(card_info[indexes[3]])
-        elif indexes[4] is not None:
-            card_object.set_attack(card_info[indexes[4]])
-        elif indexes[5] is not None:
-            card_object.set_defense(card_info[indexes[5]])
-        elif indexes[6] is not None:
-            card_object.set_edition(card_info[indexes[6]])
-        elif indexes[7] is not None:
-            card_object.set_set_number(card_info[indexes[7]])
-        elif indexes[8] is not None:
-            card_object.set_pass_code(card_info[indexes[8]])
-        elif indexes[9] is not None:
-            card_object.set_condition(card_info[indexes[9]])
-        elif indexes[10] is not None:
-            card_object.set_description(card_info[indexes[10]])
+
+        # Align card object information
+        if indexes['name'] is not None: card_object.set_name(card_info[indexes['name']])
+        if indexes['monster_type'] is not None: card_object.set_monster_type(card_info[indexes['monster_type']])
+        if indexes['attribute'] is not None: card_object.set_attribute(card_info[indexes['attribute']])
+        if indexes['level'] is not None: card_object.set_level(card_info[indexes['level']])
+        if indexes['attack'] is not None: card_object.set_attack(card_info[indexes['attack']])
+        if indexes['defense'] is not None: card_object.set_defense(card_info[indexes['defense']])
+        if indexes['pass_code'] is not None: card_object.set_pass_code(card_info[indexes['pass_code']])
+        if indexes['description'] is not None: card_object.set_description(card_info[indexes['description']])
+
+        # Align card set info with card object. There can be more than one card set per card object
+        set_info = SetInfo()
+        if indexes['set_number'] is not None: set_info.set_set_number(card_info[indexes['set_number']])
+        if indexes['edition'] is not None: set_info.set_edition(card_info[indexes['edition']])
+        if indexes['condition'] is not None: set_info.set_condition(card_info[indexes['condition']])
+        card_object.set_set_info(set_info)
+
         return card_object
 
     def extract_data(self):
@@ -88,12 +79,8 @@ class DataImport:
             for card_info in csv_reader:
                 if line_count == 0:
                     indexes = self.init_indexes(card_info)
-                elif indexes[0] is None:
-                    # TODO throw and log error. Name must be specified for card
-                    break
-                elif all(val is None for val in indexes):
-                    # TODO throw and log error. File format does not match expected format (name,monster_type,attribute,level,attack,defense,edition,set_number,pass_code,condition,description)
-                    break
+                # TODO throw and log error. Name must be specified for card
+                # TODO throw and log error. File format does not match expected format (name,monster_type,attribute,level,attack,defense,edition,set_number,pass_code,condition,description)
                 else:
                     card_object = self.define_card_object(indexes, card_info)
                     card_list.append(card_object)
