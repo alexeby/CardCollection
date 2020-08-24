573a2ae3518b9e4de308d32cd89d08bd30acdc2a
alexeby <ebyy22@gmail.com>
2020-08-23 20:53:55 -0500

    Added special character test case.

--- test/test_webhandler.py
+++ test/test_webhandler.py
@@ -1,8 +1,7 @@
-import unittest
-from carddatahandler.webhandler import WebHandler
-from carddatahandler.card import Card, SetInfo
 import time
+import unittest
 
+from carddatahandler.webhandler import WebHandler
 
 if __name__ == '__main__':
     unittest.main()
@@ -12,36 +11,27 @@ class TestGetExternalCardData(unittest.TestCase):
 
     def test_valid_card(self):
         card_name: str = 'Kuriboh'
-        card_data: dict = WebHandler.get_external_card_data(card_name)
+        card_data: dict = WebHandler.get_external_card_data_by_name(card_name)
         attack = card_data.get('atk')
         self.assertEqual(attack, 300)
         time.sleep(1)
 
     def test_invalid_card(self):
         card_name: str = 'Kuriboh1'
-        card_data: dict = WebHandler.get_external_card_data(card_name)
+        card_data: dict = WebHandler.get_external_card_data_by_name(card_name)
         self.assertIsNone(card_data)
         time.sleep(1)
 
-
-class TestValidateAndAppendCardData(unittest.TestCase):
-
-    def test_card(self):
-        set_info1: SetInfo = SetInfo(set_number='STON-ENSE1', edition='LE')
-        set_info2: SetInfo = SetInfo(set_number='DP04-EN012', edition='FE')
-        test_card: Card = Card(name='Cyber End Dragon', race='Machine', attribute='Light',
-                               monster_type='Fusion Effect Monster', level=10, attack=4000, defense=2800)
-        test_card.set_set_info([set_info1, set_info2])
-        external_data: dict = WebHandler.get_external_card_data(test_card.get_name())
-        result_card = WebHandler.validate_and_append_card_data(test_card, external_data)
-        stop = 0
+    def test_special_characters(self):
+        card_name: str = '"Winged Dragon, Guardian of the Fortress #1"'
+        card_data: dict = WebHandler.get_external_card_data_by_name(card_name)
+        attack = card_data.get('atk')
+        self.assertEqual(attack, 1400)
+        time.sleep(1)
 
 
-class TestUsePopulatedData(unittest.TestCase):
-    #TODO
-    pass
+class TestDownloadImg(unittest.TestCase):
 
-
-class TestHandleSetData(unittest.TestCase):
-    #TODO
-    pass
+    def test_successful_img_download(self):
+        # TODO
+        pass
